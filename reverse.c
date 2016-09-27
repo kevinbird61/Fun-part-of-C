64dc97f881182a7c7ad1de7e31f549cc74c5bd84
kevinbird61 <kevinbird61@gmail.com>
2016-09-28 00:11:54 +0800

    Upload for the reversing string's version.
    The condition:
    1. In-place reverse
    2. Need recursive.

---
+++ reverse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void swap_char(char *a,char *b){
+  *a ^= *b;
+  *b ^= *a;
+  *a ^= *b;
+}
+
+void recurse_reverse(char *head,char *tail){
+  if(head >= tail) return;
+  swap_char(head,tail);
+  recurse_reverse(head+=sizeof(char),tail-=sizeof(char));
+}
+
+int main(void){
+  char *str = malloc(1*sizeof(char));
+  scanf("%s",str);
+  recurse_reverse(str,(str+strlen(str)-1));
+  printf("%s , %d\n",str,(int)strlen(str));
+}
